73328f6845a9d69d6ba19547331ddfeac1ef2846
Alison Woodman <aliwoodman@gmail.com>
2017-06-13 17:16:08 +0200

    ch2ex2

---
+++ session2/alison-answers/ch2ex2.rb
@@ -0,0 +1,31 @@
+# Prompt the user for a number, then read it in and print out "hi" that many times
+# 
+# Repeat this process until the user submits "bye", then say "goodbye" and end the program
+# HINT: Check out example 2 if you get stuck
+
+# example:
+# PROGRAM: Enter a number
+# USER:    4
+# PROGRAM: hi hi hi hi
+# PROGRAM: Enter a number
+# USER:    2
+# PROGRAM: hi hi
+# PROGRAM: Enter a number
+# USER:    bye
+# PROGRAM: goodbye
+
+
+# remember you can try your program out with              $ ruby 2_input_output_control.rb
+# and when you think it is correct, you can test it with  $ rake 2:2
+
+def hi_hi_goodbye
+ number = ""
+
+  while number != "bye"
+  	puts "Pick a number"
+  number = gets.chomp
+  number.to_i.times do puts "Hi"
+  end
+end
+  	puts "goodbye"
+end
