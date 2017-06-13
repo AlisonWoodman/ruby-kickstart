5f65142ff3d8e97d1205f6d85363b79dbf27911e
Alison Woodman <aliwoodman@gmail.com>
2017-06-13 13:01:27 +0200

    ch1ex7

---
+++ session1/alison-answers/ch1ex7.rb
@@ -0,0 +1,17 @@
+# given a string, return the character after every letter "r"
+# 
+# pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
+# pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
+# pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
+
+def pirates_say_arrrrrrrrr(string)
+	 to_return = ""
+	 n = ((string.size) -1 )
+  n.times do |index|
+    if (string[index] == "r" || string[index] == "R")
+    		to_return << string[(index + 1)]
+	else
+	end
+  end
+  print to_return
+end
