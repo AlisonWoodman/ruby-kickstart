fea7dcc13e142e62cc91a03ab1f740121902a21d
Alison Woodman <aliwoodman@gmail.com>
2017-06-14 15:17:30 +0200

    ch2ex8

---
+++ session2/alison-answers/ch2ex8.rb
@@ -0,0 +1,19 @@
+# Given an array of elements, return true if any element shows up three times in a row
+# 
+# Examples:
+# got_three? [1, 2, 2, 2, 3]  # => true
+# got_three? ['a', 'a', 'b']  # => false
+# got_three? ['a', 'a', 'a']  # => true
+# got_three? [1, 2, 1, 1]     # => false
+
+def got_three?(array)
+new_array = []
+answer = false
+	array.each_cons(3) do |a, b, c| 
+   	  if a == b && b == c
+      answer = true
+      else
+      end
+    end
+    print answer
+end
